3e994b7dca5bae75db0ea452d43537eedbcd6fa6
AhmedElsalhy <142514288+AhmedElsalhy@users.noreply.github.com>
2023-08-22 18:40:46 +0300

    post request

---
+++ lib/services/signin.dart
@@ -0,0 +1,17 @@
+import 'package:vacations_app/helper/api.dart';
+import 'package:vacations_app/model.dart';
+
+class SignIn {
+  Future<Model> signin(
+      {required String username,
+        required String password}) async {
+    Map<String, dynamic> data = await Api().post(
+      url: 'http://34.243.128.248:8080/VHS_API_CHAT_BOT/api',
+      body: {
+        "username": username,
+        "password": password,
+      },
+    );
+    return Model.fromJson(data);
+  }
+}
